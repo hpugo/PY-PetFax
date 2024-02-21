633a6a87a16cc45e2733dbabab36fd4418b46474
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2024-02-20 19:34:30 -0600

    adding the routes

---
+++ petfax/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello():
+        return 'Hello!'
+    
+    @app.route('/pets')
+    def pets():
+        return 'These are our pets available for adoption!'
+
+    return app
